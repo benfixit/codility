89c934300191fb44cc96b7c9473b9720c8cb07b5
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2019-09-29 20:13:48 +0100

    Add reverse array improved

--- arrays/exercise.js
+++ arrays/exercise.js
@@ -24,6 +24,55 @@ function reverseArrayWithoutAnyHelper(arr){
     return newArr
 }
 
+function swap(arr, firstIndex, secondIndex){
+    const temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+}
+
+function experimentalReverseArray(arr){
+    const midPoint = Math.floor(arr.length/2);
+    const bar = midPoint - 1;
+    const arrLength = arr.length
+    for(let i = 0; i <= bar ; ++i){
+        swap(arr, i, arrLength - 1 - i)
+    }
+    return arr;
+}
+
+function generateArray(numberOfElements){
+    const arr = [];
+    for(let i = 0; i < numberOfElements; ++i){
+        arr.push(Math.floor(Math.random() * 101))
+    }
+    return arr;
+}
+
+function displayArray(arr){
+    let str = "";
+    const arrLength = arr.length;
+    for(let i = 0; i < arrLength; ++i){
+        if(i > 0 && i % 10 === 0){
+            str += arr[i] + '\n';
+        } else{
+            str += arr[i] + ' ';
+        }
+    }
+    return str;
+}
+
+const arr = generateArray(10000000);
+console.log(':::Generated Array:::')
+displayArray(arr)
+
+const start = new Date().getTime()
+console.log('\n\n!!!Starting Array Reversal!!!')
+
+console.log('\n\n:::Reversed Array:::')
+displayArray(experimentalReverseArray(arr))
+const end = new Date().getTime()
+const timeSpent = end - start;
+console.log(`${timeSpent} ms`)
 console.log(reverseArray([2, 45, 12, 10, 1, 100]))
 console.log(reverseArrayWithoutReverseHelper([2, 45, 12, 10, 1, 100]))
 console.log(reverseArrayWithoutAnyHelper([2, 45, 12, 10, 1, 100]))
